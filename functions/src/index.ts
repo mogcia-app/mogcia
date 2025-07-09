03e72da6a2143607e5dcbcb9c597e58cf213d8c9
marina ishida <mmmrn0@icloud.com>
2025-07-09 14:51:01 +0900

    remove unused firebase functions

--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as functions from "firebase-functions";
-
-// 🚀 テストAPI
-export const helloWorld = functions.https.onRequest((request, response) => {
-  console.log("🐛 helloWorld endpoint hit!");
-  response.send("Hello from Firebase Functions!");
-});
