4bf42320513b68e5226c94c44cccd7ec0e310685
marina ishida <mmmrn0@icloud.com>
2025-07-10 11:41:10 +0900

    Add GA4 and site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
 import "./globals.css";
 import { ReactNode } from "react";
-import Head from "next/head";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
-      <Head>
+      <head>
         <meta name="google-site-verification" content="GTyUSH40uJTk6SwF3mkDTrQU7ekuxHPe9EIvEDrHuWk" />
-      </Head>
+      </head>
       <body>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-R8LMDVT08M" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R8LMDVT08M');
+          `}
+        </Script>
+
         <header className="flex justify-between items-center px-8 py-4 bg-white/30 backdrop-blur-md shadow">
           <div className="text-[15px] font-bold text-gray-800">
             <Link href="/">MOGCIA</Link>
