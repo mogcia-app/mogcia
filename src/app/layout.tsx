4ef9f7ebe58bf51e3fe5b15fe2cef1c806d6cfbd
marina ishida <mmmrn0@icloud.com>
2025-07-10 11:31:39 +0900

    Add Google site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
-// app/layout.tsx
 import Link from "next/link";
 import "./globals.css";
+import { ReactNode } from "react";
+import Head from "next/head";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
+      <Head>
+        <meta name="google-site-verification" content="GTyUSH40uJTk6SwF3mkDTrQU7ekuxHPe9EIvEDrHuWk" />
+      </Head>
       <body>
         <header className="flex justify-between items-center px-8 py-4 bg-white/30 backdrop-blur-md shadow">
-  <div className="text-[15px] font-bold text-gray-800">
-    <Link href="/">MOGCIA</Link>
-  </div>
-  <nav className="space-x-6">
-    <Link href="/contact" className="hover:text-gray-600">お問い合わせ</Link>
-  </nav>
-</header>
+          <div className="text-[15px] font-bold text-gray-800">
+            <Link href="/">MOGCIA</Link>
+          </div>
+          <nav className="space-x-6">
+            <Link href="/contact" className="hover:text-gray-600">お問い合わせ</Link>
+          </nav>
+        </header>
         <main>{children}</main>
       </body>
     </html>
